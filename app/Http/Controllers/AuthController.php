bd856285233d6b3d8fe6ef782ef0fec548e006ce
nadiapputrii <nadiapputriirah@gmail.com>
2021-10-15 14:31:28 +0700

    auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,16 +4,60 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
 class AuthController extends Controller
 {
-    public function me()
+
+    public function index()
     {
         return[
             'NIS' => '3103119133',
             'Name' => 'Nadia Putri Rahmaniar',
             'Gender' => 'Perempuan',
-            'Phone' => '08960033496',
+            'Phone' => '08966896498',
             'Class' => 'XII RPL 4',
         ];
+    }
+
+    public function me(Request $request)
+    {
+    return $request->user();
+    }
+
+    public function register(Request $request)
+    {
+        $user = User::create([
+            'name' => $request['name'],
+            'email' => $request['email'],
+            'password' => Hash::make($request['password']),
+        ]);
+    
+        $token = $user->createToken('auth_token')->plainTextToken;
+    
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+
+    public function login(Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+        return response()->json([
+        'message' => 'Invalid login details'
+                ], 401);
+            }
+
+        $user = User::where('email', $request['email'])->firstOrFail();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+                'access_token' => $token,
+                'token_type' => 'Bearer',
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookController;
-use App\Http\Controllers\AuthorController;
+// use App\Http\Controllers\AuthorController;
 use GuzzleHttp\Promise\Create;
 
 /*
@@ -23,18 +23,22 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/me', [AuthController::class, 'me']);
+Route::post('/me', [AuthController::class, 'me'])->middleware('auth:sanctum');
 
 Route::get('/book', [BookController::class, 'index']);
-Route::post('/book', [BookController::class, 'store']);
+Route::post('/book', [BookController::class, 'store'])->middleware('auth:sanctum');
 Route::get('/book/{id}', [BookController::class, 'show']);
-Route::put('/book/{id}', [BookController::class, 'update']);
-Route::delete('/book/{id}', [BookController::class, 'destroy']);
-Route::resource('book', BookController::class)->except('edit', 'create');
+Route::put('/book/{id}', [BookController::class, 'update'])->middleware('auth:sanctum');
+Route::delete('/book/{id}', [BookController::class, 'destroy'])->middleware('auth:sanctum');
+// Route::resource('book', BookController::class)->except('edit', 'create');
 
-Route::get('/author', [AuthorController::class, 'index']);
-Route::post('/author', [AuthorController::class, 'store']);
-Route::get('/author/{id}', [AuthorController::class, 'show']);
-Route::put('/author/{id}', [AuthorController::class, 'update']);
-Route::delete('/author/{id}', [AuthorController::class, 'destroy']);
-Route::resource('author', AuthorController::class)->except('edit', 'create');
+// Route::get('/author', [AuthorController::class, 'index']);
+// Route::post('/author', [AuthorController::class, 'store']);
+// Route::get('/author/{id}', [AuthorController::class, 'show']);
+// Route::put('/author/{id}', [AuthorController::class, 'update']);
+// Route::delete('/author/{id}', [AuthorController::class, 'destroy']);
+Route::resource('author', "\App\Http\Controllers\AuthorController")->except('edit', 'create')->middleware('auth:sanctum');
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
